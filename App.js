b2c012bccd93837b9b1138b88caedb08138084f0
bou-said <said.bouhdifi@gmail.com>
2022-05-27 21:14:17 +0200

    First Commit Added carImages

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {HomeScreen} from "./screens/HomeScreen";
+import {Navigation} from "./components/Navigation";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
     </View>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, StyleSheet} from "react-native";
+
+export function HomeScreen(){
+    return(
+        <Text>
+            Homescreen
+        </Text>
+    )
+
+}
